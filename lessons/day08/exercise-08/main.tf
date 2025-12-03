06d7b694c64a68678751c4d0c3289cf83096886e
sartajhajam <hajamsaj@gmail.com>
2025-12-03 14:13:04 +0530

    Adjust exercise-08 variables and main for for_each demo

--- lessons/day08/exercise-08/main.tf
+++ lessons/day08/exercise-08/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 # ---------------------------
 # TASK 1: CREATE BUCKETS USING COUNT
 # ---------------------------
-resource "aws_s3_bucket" "count_buckets" {
+/* resource "aws_s3_bucket" "count_buckets" {
   count = length(var.bucket_names_count)
 
   # Each bucket name selected using its index
@@ -15,4 +15,20 @@ resource "aws_s3_bucket" "count_buckets" {
     CreatedBy = "COUNT demo"
     Index     = count.index
   }
+} */
+
+# ---------------------------
+# TASK 2: CREATE BUCKETS USING FOR_EACH
+# ---------------------------
+resource "aws_s3_bucket" "foreach_buckets" {
+  for_each = toset(var.bucket_names_each)
+
+  # each.value contains the current bucket name
+  bucket = each.value
+
+  tags = {
+    CreatedBy = "FOR_EACH demo"
+    Name      = each.value
+  }
 }
+
--- lessons/day08/exercise-08/variables.tf
+++ lessons/day08/exercise-08/variables.tf
@@ -1,5 +1,13 @@
-# A list of bucket names (order matters)
-variable "bucket_names_count" {
+# A list of bucket names (order matters) for Task 1 
+/* variable "bucket_names_count" {
   type        = list(string)
   description = "Bucket names to create using count"
+}
+ */
+
+ # Task 2 Create two S3 buckets using for_each with set(string).
+ # A set of bucket names (order does NOT matter)
+variable "bucket_names_each" {
+  type        = set(string)
+  description = "Bucket names to create using for_each"
 }
